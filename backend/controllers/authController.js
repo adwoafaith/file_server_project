18586709dcd799dcb77c3b178c1f80149efc11e4
adwoafaith <adwoafaith49@gmail.com>
2023-07-07 10:17:17 +0000

    finishing of the resetpassword route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,164 +1,185 @@
-const User = require('../models/user')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const sendEmail = require('../utils/email')
-const businessDistribution = require('../models/file')
-const { genToken } = require('../utils/access-token')
-const crypto = require('crypto')
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+//const sendEmail = require('../utils/email')
+const businessDistribution = require("../models/file");
+const { genToken, verifyToken } = require("../utils/access-token");
+const crypto = require("crypto");
+const nodemailer = require("nodemailer");
+require('dotenv').config()
 //handle errors
 const handleErrors = (err) => {
-    // console.log('heelo', err.message, err.code)
-    let errors = { email: '', password: '' };
-    
-    //handeling duplicate error code
-    if (err.code === 11000) return { message: 'That email is already registered' }
+  // console.log('heelo', err.message, err.code)
+  let errors = { email: "", password: "" };
 
-    //validation errors   
-    if (err.message.includes('User validation failed')) {
-        Object.values(err.errors).forEach(({ properties }) => {
-            errors[properties.path] = properties.message
-        });
-    }
-    return {message: errors.password};
-}
+  //handeling duplicate error code
+  if (err.code === 11000)
+    return { message: "That email is already registered" };
+
+  //validation errors
+  if (err.message.includes("User validation failed")) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return { message: errors.password };
+};
 
 //registration function
 const signup_post = async (req, res, next) => {
-    const { email, password } = req.body;
-    try {
-        const user = await User.create({ email, password });
-        res.status(201).json({ message: 'Registered sucessfully' });
-    }
-    catch (err) {
-        res.status(400).json({ ...handleErrors(err) })
-    }
-
-}
+  const { email, password } = req.body;
+  //console.log(email,password)
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json({ message: "Registered sucessfully" });
+  } catch (err) {
+    res.status(400).json({ ...handleErrors(err) });
+  }
+};
 
 const login = async (req, res, next) => {
-    let { email, password } = req.body;
+  const { email, password } = req.body;
+  try {
     //checking if user provided username and password
-    if (!email || !password) {
-        return res.status(400).json({
-            message: "username or password not present"
-        })
+    const notUser = !email || !password;
+    console.log("rtt");
+    if (notUser) {
+      return res.status(400).json({
+        message: "username or password not present",
+      });
     }
-    try {
-        const user = await User.findOne({ email })
-        if (!user) {
-            return res.status(400).json({
-                message: "login not successful",
-                error: "user not found"
-            });
-        } else {
-            //compare given password to hashed password
-            bcrypt.compare(password, user.password).then(function (result) {
-                if (result) {
-                    let token = genToken({email,  role:user.role})
-                    return res.status(200).json({
-                        message: "login Successful",
-                        token              
-                    });
-                }
-                else {
-                    return res.status(400).json({
-                        message: "Login not successfull"
-                    });
-                }
-
-            })
-        }
-
-    }
-    catch (error) {
-        res.status(400).json({
-            message: "An error occured",
-            error: error.message
-        })
-    }
-}
-const forgotPassword = async(req, res, next) =>{
-     //getting the user based on the provided email
-     const {email} = req.body;
-     console.log(email)
-     console.log(email)
-     //find the user by email
-     const user = await User.findOne({email})
-     console.log(email)
-     if (!user){
-        return res.status(404).json({
-            message: "No user exists with email"
-        })
-     }
-     //generate a random reset token
-     const resetToken = user.createResetPasswordToken();
-     await user.save({validateBeforeSave:false});
-     //send the token to the user email
-     const resetUrl = `http://localhost:8000/user/resetPassword/${resetToken}\n\n`
-     const message = `we have received a password request.Use the below link to reset password \n\n${resetUrl}\n\n link is available for only 10 minute`;
-     try{
-    
-         await sendEmail({
-            email:user.email,
-            subject:'Password change request received',
-            message:message
-         });
-         res.status(200).json({status:'success',
-         message:`Password reset link send to the user email`})
-     }
-     catch(error){
-        user.passwordResetToken = undefined;
-        user.passwordResetTokenExpires = undefined
-        user.save({validateBeforeSave:false});
-    
-        res.status(500).json({message:`There was an error sending password reset email.Please try again later`,error:error.message})
-     }
-      
-    }
-    console.log(sendEmail)
-    
-    const resetPassword = async(req, res, next) => {
-        const token = crypto.createHash('sha256').update(req.params.token).digest('hex');
-        console.log(token)
-        const user = await User.findOne({passwordResetToken:token,passwordResetTokenExpires:{$gt:Date.now()}})
-try {
-         if (!user)
-        {
-            res.status(400).json({
-                message: 'token is invalid or has expired'
-            })
-        }
-        //reseting the password
-        user.password = req.body.password;
-        user.passwordResetToken = undefined;
-        user.passwordResetTokenExpires = undefined;
-        user.passwordChangedAt = Date.now();
-        user.save();
-
-        //after resetting the password login the user automatically
-        const loginToken = genToken({email,role:user.role})
-        return res.status(200).json({
+    const user = await User.findOne({ email });
+    console.log(user);
+    if (!user) {
+      return res.status(400).json({
+        message: "login not successful",
+        error: "user not found",
+      });
+    } else {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) return next(err);
+        console.log(result);
+        if (result) {
+          let token = genToken({ email, role: user.role });
+          return res.status(200).json({
             message: "login Successful",
-            token :loginToken          
-        });   
-                
-} catch (error) {
-                res.status(400).json({
-                    message:'something went wrong',
-                    error:error.message
-                })
-                
-            }
-        
+            token,
+          });
+        } else {
+          return res.status(400).json({
+            message: "Login not successfull",
+          });
+        }
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "An error occured",
+      error: error.message,
+    });
+    console.log(error);
+  }
+};
+
+const forgotPassword = async (req, res) => {
+  const {email} = req.body;
+  console.log(email)
+  try {
+    //generate a unique token
+    const token = crypto.randomBytes(20).toString("hex");
+    //console.log(token)
+
+    //find the user by their email
+    const user = await User.findOne({email});
+    console.log(user)
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User is not found" });
+    }
+    //store token and generate tokenexpires in user records
+    user.passwordResetToken = token;
+    user.passwordResetTokenExpires = Date.now() + 3600000;
+    await user.save();
+
+    const mail_name = process.env.USER_NAME;
+    const password = process.env.APP_PASSWORD;
+
+    //transporter create a transporter
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: mail_name,
+        pass: password,
+      },
+    });
+    //email options
+    const mailOptions = {
+      from: mail_name,
+      to: user.email,
+      subject: "Reset password",
+      text:
+        `You or someone send a request to change your email password` +
+        `ignore of not necessary or click on the link below to reset your password:\n\n` +
+        `http://localhost:8000/user/resetPassword/${token}\n\n`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res
+          .status(500)
+          .json({ sucess: false, message: "Email not send! Failed" });
+      }
+      return res
+        .status(200)
+        .json({
+          sucess: true,
+          message: "Password link sent to the email provided",
+        });
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+};
+
+const resetPassword = async (req, res) => {
+  const { token } = req.params;
+  console.log(token)
+  console.log(password)
+  try {
+    //find user by toke and the duration the token expires
+    const user = await User.findOne({
+      passwordResetToken: token,
+      passwordResetTokenExpires: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "token is invalid or has expired" });
     }
 
+    //reseting the password
+    user.password = password;
+    user.passwordResetToken = undefined;
+    user.passwordResetTokenExpires = undefined;
+    await user.save();
 
+    return res
+      .status(200)
+      .json({ sucess: true, message: "Password reset sucessful" });
+  } catch (error) {
+    res.status(400).json({
+      message: "something went wrong",
+      error: error.message,
+    });
+  }
+};
 
-module.exports =
-{
-    login,
-    signup_post,
-    forgotPassword,
-    resetPassword
-
-    }
+module.exports = {
+  login,
+  signup_post,
+  forgotPassword,
+  resetPassword,
+};
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose')
 const {isEmail} = require('validator')
 const connect = require('mongoose').connect
 const crypto = require('crypto');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcrypt')
 const { reset } = require('nodemon');
 const Schema = mongoose.Schema;
 
@@ -18,35 +18,24 @@ const userSchema = new Schema ({
     password:{
         type: String,
         required: [true,'Please enter a password'],
-        minlength:[6,'Minimum password length is 6 characters'],
+        minlength:[6,'Minimum password length is 6 characters']
     },
     role:{
         type: String,
         enum :['user','admin'], 
         default: 'user'
-    },
+    }, 
     passwordChangedAt:Date,
     passwordResetToken: String,
     passwordResetTokenExpires:Date
 },{timestamps:true})
 
-//instance method
-userSchema.methods.createResetPasswordToken = function(){
-const resetToken = crypto.randomBytes(32).toString('hex');
-
-this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-this.passwordResetTokenExpires = Date.now()+ 10*60*3000;
-
-console.log(resetToken,this.ResetPasswordToken)
-return resetToken;
-}
-
 //fire a function after doc saved to db
-userSchema.post('save',function(doc,next){
-    console.log('new user was created & saved',doc)
-    next();
-})
-//hasing the user password
+// userSchema.post('save',function(doc,next){
+//     console.log('new user was created & saved',doc)
+//     next();
+// })
+// //hasing the user password
 userSchema.pre('save',async function(next) {
    const salt = await bcrypt.genSalt();
    this.password = await bcrypt.hash(this.password,salt)
